6815df44b88f0e9c4cc686901d7c2a57daff2bf9
itachi2o <maymartinmichel@gmail.com>
2023-11-09 23:15:33 -0600

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   main-js/panel.js

--- main-js/panel.js
+++ main-js/panel.js
@@ -2,4 +2,4 @@ fondoImagen.style.backgroundImage = "url(./img/pastel2.png)";//imagen de fondo
 colorFondo.style.backgroundColor = "#212529";//color del fondo
 
 titulo.innerHTML = "Mis pasteles";//titulo
-tituloColor.style.color = "white";//color del titulo
+tituloColor.style.color = "black";//color del titulo
